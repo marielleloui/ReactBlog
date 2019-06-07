af860259270a9549efc325ff567ce3003702804c
Marielle Louise Cruz <mariellecruz@yahoo.com>
2019-06-07 13:21:12 +1000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./components/Post/post";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+    </div>
+  );
+}
+
+export default App;
